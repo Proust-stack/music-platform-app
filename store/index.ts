ba1fdc65140d15566b44d826c96cd0e3ab7a9031
Andrey_B <example@gmail.com>
2021-08-27 09:56:49 +0300

    changed wrapper

--- store/index.ts
+++ store/index.ts
@@ -5,9 +5,9 @@ import { reducer, RootState } from "./reducers";
 
 
 // create a makeStore function
-const makeStore = (context: Context) => createStore(reducer, applyMiddleware(thunk));
+const makeStore: MakeStore<RootState> = (context: Context) => createStore(reducer, applyMiddleware(thunk));
 
 // export an assembled wrapper
-export const wrapper = createWrapper<Store<RootState>>(makeStore, {debug: true});
+export const wrapper = createWrapper<RootState>(makeStore, {debug: true});
 
 export type NextThunkDispatch = ThunkDispatch<RootState, void, AnyAction>
